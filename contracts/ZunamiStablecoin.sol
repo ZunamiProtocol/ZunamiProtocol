dc64e821d516436a34ea582912f414ec61c8d1fb
Notorious88 <voodo7771@gmail.com>
2021-04-01 15:17:57 +0300

    fix(product): delete Overflow
    delete Overflow modifier

--- contracts/ZunamiStablecoin.sol
+++ contracts/ZunamiStablecoin.sol
@@ -16,18 +16,12 @@ contract ZunamiStablecoin is ERC20{
         _mint(_address, _amount);
     }
 
-    function burn(address _address, uint256 _amount) Overflow(_amount) onlyOwner() external {
+    function burn(address _address, uint256 _amount) onlyOwner() external {
         _burn(_address, _amount);
     }
 
     modifier onlyOwner () {
         require(msg.sender == owner, 'only owner');
-        _;
-    }
-
-    modifier Overflow (uint _amount) {
-        uint _totalSupply = totalSupply();
-        require(_totalSupply - _amount > 0, 'Overflow!');
         _;
     }
 
