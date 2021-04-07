aad96090be58a8c4e1c4cbf059fca12377867e35
Notorious88 <voodo7771@gmail.com>
2021-04-07 18:47:47 +0300

    feat(product): send trasfer to contract and get balance contract
    test MetaMask
    send coin to contract ZunamiStablecoin
    get balance contract ZunamiStablecoin

--- client/src/App.js
+++ client/src/App.js
@@ -2,30 +2,38 @@ import React, {useEffect, useState} from 'react';
 
 import { getWeb3, getZunami } from './tools/utils';
 
-
 const App = () => {
     const [web3, setWeb3] = useState(undefined);
     const [accounts, setAccounts] = useState(undefined);
     const [zunami, setZunami] = useState(undefined);
+    const [zunamiAddress, setZunamiAddress] = useState(undefined);
 
     useEffect(() =>{
         (async () => {
             const web3 = await getWeb3();
             const accounts = await web3.eth.getAccounts();
             const zunami = await getZunami(web3);
+
             setWeb3(web3);
             setAccounts(accounts);
             setZunami(zunami);
+            setZunamiAddress(zunami._address);
         })();
     })
 
-    console.log(web3, 'web3');
-    console.log(accounts, 'acc');
-    console.log(zunami, 'zunami');
+    const sendTransfer = async () => {
+        await zunami.methods.mint(zunamiAddress, 10000).send({from: accounts[0]});
+    }
+
+    const getBalance = async () => {
+        let balance = await zunami.methods.balanceOf(zunamiAddress).call();
+        console.log(balance,'balance contract');
+    }
 
   return(
     <>
-        <h1>Hello World</h1>
+        <button onClick={sendTransfer} >transfer</button>
+        <button onClick={getBalance} >balance</button>
     </>
   )
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
 const Zunami = artifacts.require("ZunamiStablecoin");
 
-module.exports = async (deployer, _network, accounts) => {
-    await deployer.deploy(Zunami, accounts[0]);
-    const wallet = await Zunami.deployed();
+module.exports = async function(deployer) {
+  await deployer.deploy(Zunami);
+  await deployer.deploy(accounts[0]);
 };
