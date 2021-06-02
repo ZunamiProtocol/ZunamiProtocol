ef9abfa6f3fa8018c99bf90f777ed7a6b969195a
Notorious88 <voodo7771@gmail.com>
2021-06-02 16:54:29 +0300

    refactor(product): changes in the logic of the gitPush.js script
    change how error handling works

--- gitPush.js
+++ gitPush.js
@@ -1,18 +1,24 @@
 const {exec} = require('child_process');
 
-exec('git rev-parse --abbrev-ref HEAD', (error, stdout, stderr) => {
-    if (error) {
-        console.log(error);
-    }
-    exec(`git push --follow-tags origin ${stdout.replace('\n', '')}`, (error, stdout, stderr) => {
-        if (error) {
-            console.log(error);
-        }
-        if (stderr) {
-            console.log(stderr);
-        }
+const executeСommand = (cammand) => {
+    return new Promise((resolve) => {
+        exec(cammand, (error, stdout, stderr) => {
+            if (error) {
+                console.log(error);
+            }
+            if (stdout) {
+                return resolve(stdout);
+            }
+            if (stderr) {
+                console.log(stderr);
+            }
+        });
     });
-    if (stderr) {
-        console.log(stderr);
-    }
-});
+};
+
+const getNameBranchAndPush = async () => {
+    const nameBranch = await executeСommand('git rev-parse --abbrev-ref HEAD');
+    await executeСommand(`git push --follow-tags origin ${nameBranch.replace('\n', '')} `);
+};
+
+getNameBranchAndPush();
