103c5300503e8ac1c11cf9614cc22a23995ad2f5
Notorious88 <voodo7771@gmail.com>
2021-06-02 17:23:40 +0300

    refactor(product): change argument name

--- gitPush.js
+++ gitPush.js
@@ -1,8 +1,8 @@
 const {exec} = require('child_process');
 
-const executeСommand = (cammand) => {
+const executeСommand = (command) => {
     return new Promise((resolve) => {
-        exec(cammand, (error, stdout, stderr) => {
+        exec(command, (error, stdout, stderr) => {
             if (error) {
                 console.log(error);
             }
