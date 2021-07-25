647cc9816f0adb79ab9eb98c9bcb13e24074a7d0
mulili <mulili.fb@gmail.com>
2021-07-25 21:04:09 +0300

    fixed testing bugs

--- test/1_vault.js
+++ test/1_vault.js
@@ -1,31 +1,48 @@
-require("dotenv").config();
+const {expect} = require('chai');
+const {waffle} = require('hardhat');
 
-const { expect } = require('chai');
-const { waffle } = require('hardhat');
-const provider = waffle.provider;
+// get the signers
+let owner; let addr1; let zunamiVaultContract; let testUSDTContract;
 
 const setup = async () => {
-  const ZunamiVault = await ethers.getContractFactory('Vault');
-  const zunamiVault = await ZunamiVault.deploy();
-  await zunamiVault.deployed();
-  return zunamiVault;
+    // deploy the test USDT first
+    const TestUSDT = await ethers.getContractFactory('USDTTestCoin');
+    testUSDTContract = await (await TestUSDT.deploy()).deployed();
+
+
+    // then deploy the vault
+    const ZunamiVault = await ethers.getContractFactory('Vault');
+    const zunamiVault = await ZunamiVault.deploy(testUSDTContract.address);
+    await zunamiVault.deployed();
+    [owner, addr1, ...addrs] = await ethers.getSigners();
+    return zunamiVault;
 };
 
 
-describe('Deploying the vault contract', () => {
-
-  before(async () => {
-    zunamiVaultContract = await setup();
-  });
-
-  describe('Test vault contract deployment', () => {
-
-    it('Should set the deployed vault to the correct owner', async function () {
-    
+describe('Deploying the vault + USDT contracts', () => {
+    before(async () => {
+        zunamiVaultContract = await setup();
     });
 
-    
-   
-  });
+    describe('Test vault contract deployment', () => {
+        it('Should set the deployed vault to the correct owner', async () => {
+            expect(await zunamiVaultContract.owner()).to.equal(owner.address);
+            expect(await testUSDTContract.owner()).to.equal(owner.address);
+        });
+    });
 
+    describe('Test vault deposit and withdrawal', () => {
+        it('User should be able to deposit to the vault and get LP Tokens', async () => {
+            const usdtDepositAmount = ethers.utils.parseEther('100');
+            const lpTokensBal = await zunamiVaultContract.deposit(usdtDepositAmount);
+            console.log(lpTokensBal);
+            expect(lpTokensBal).to.be.gte(usdtDepositAmount);
+        });
+
+        // eslint-disable-next-line max-len
+        it('User should be able to withdraw from the vault, burn the LP Tokens & get their initial deposit back', async () => {
+            //  expect(await zunamiVaultContract.owner()).to.equal(owner.address);
+            //   expect(await testUSDTContract.owner()).to.equal(owner.address);
+        });
+    });
 });
