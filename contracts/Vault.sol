0bb6cd9651b261db4662b76ed2c0d69eb0a3b60d
mulili <mulili.fb@gmail.com>
2021-07-25 17:42:40 +0300

    added a test usdt contract

---
+++ contracts/Vault.sol
@@ -0,0 +1,74 @@
+pragma solidity ^0.5.17;
+
+import "@openzeppelinV2/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelinV2/contracts/math/SafeMath.sol";
+import "@openzeppelinV2/contracts/utils/Address.sol";
+import "@openzeppelinV2/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelinV2/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelinV2/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelinV2/contracts/ownership/Ownable.sol";
+
+
+contract yVault is ERC20, ERC20Detailed {
+    using SafeERC20 for IERC20;
+    using Address for address;
+    using SafeMath for uint256;
+
+    IERC20 public token;
+
+    constructor(address _token)
+        public
+        ERC20Detailed(
+            string(abi.encodePacked("zunami ", ERC20Detailed(_token).name())),
+            string(abi.encodePacked("zun", ERC20Detailed(_token).symbol())),
+            ERC20Detailed(_token).decimals()
+        )
+    {
+        token = IERC20(_token);
+        
+    }
+
+    function balance() public view returns (uint256) {
+        return token.balanceOf(address(this));
+    }
+
+
+    // Custom logic in here for how much the vault allows to be borrowed
+    // Sets minimum required on-hand to keep small withdrawals cheap
+    function available() public view returns (uint256) {
+        return token.balanceOf(address(this)).mul(min).div(max);
+    }
+
+    function depositAll() external {
+        deposit(token.balanceOf(msg.sender));
+    }
+
+    function deposit(uint256 _amount) public {
+        uint256 _pool = balance();
+        
+        uint256 shares = 0;
+        if (totalSupply() == 0) {
+            shares = _amount;
+        } else {
+            shares = (_amount.mul(totalSupply())).div(_pool);
+        }
+        _mint(msg.sender, shares);
+    }
+
+    function withdrawAll() external {
+        withdraw(balanceOf(msg.sender));
+    }
+
+    // Allow user to withdraw their initial deposit and burn the LP Tokens
+    function withdraw(uint256 _shares) public {
+        uint256 r = (balance().mul(_shares)).div(totalSupply());
+        _burn(msg.sender, _shares);
+
+        // Transfer the token
+        token.safeTransfer(msg.sender, r);
+    }
+
+    function getPricePerFullShare() public view returns (uint256) {
+        return balance().mul(1e18).div(totalSupply());
+    }
+}
---
+++ test/1_vault.js
@@ -0,0 +1,20 @@
+const { expect } = require('chai');
+
+describe('Deploying the vault contract', () => {
+
+
+  before(async () => {
+   
+  });
+
+  describe('Test vault contract deployment', () => {
+
+    it('Should set the deployed contracts to the correct owner', async function () {
+    
+    });
+
+    
+   
+  });
+
+});
