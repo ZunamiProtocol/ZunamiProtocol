421bb28b764c9ffc2df49b2680113fdba7954742
mulili <mulili.fb@gmail.com>
2021-07-27 14:59:48 +0300

    Rebsase from correct branch

--- contracts/ZunamiVault.sol
+++ contracts/ZunamiVault.sol
@@ -1,13 +1,68 @@
-//SPDX-License-Identifier: MIT
-pragma solidity 0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+pragma experimental ABIEncoderV2;
 
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract ZunamiVault {
-    function deposit(address _depositer, uint _amount, bytes32 _ticker) external {}
+contract ZunamiVault is ERC20 {
 
-    function withdrawAll(address _depositer, int128 _coin, uint _minAmount,
-        bytes32 _ticker) external {}
+    using SafeERC20 for IERC20;
+    using Address for address;
+    using SafeMath for uint256;
 
-    function withdraw(address _depositer, uint _amount, bytes32 _ticker) external {}
+    IERC20 public token;
+
+    // for now it is going to be the address that deploys this contract but in the future it has to be a DAO
+    address public owner;
+
+    constructor(address _token)
+        ERC20(
+            string(abi.encodePacked("zunami", ERC20(_token).name())),
+            string(abi.encodePacked("zun", ERC20(_token).symbol()))
+        )
+    {
+        token = IERC20(_token);
+        owner = msg.sender;
+    }
+
+    function balance() public view returns (uint256) {
+        return token.balanceOf(address(this));
+    }
+    function depositAll() external {
+        deposit(token.balanceOf(msg.sender));
+    }
+
+    function deposit(uint256 _amount) public {
+        uint256 _pool = balance();
+        
+        uint256 shares = 0;
+        if (totalSupply() == 0) {
+            shares = _amount;
+        } else {
+            shares = (_amount.mul(totalSupply())).div(_pool);
+        }
+        _mint(msg.sender, shares);
+    }
+
+    function withdrawAll() external {
+        withdraw(balanceOf(msg.sender));
+    }
+
+    // Allow user to withdraw their initial deposit and burn the LP Tokens
+    function withdraw(uint256 _shares) public {
+        uint256 r = (balance().mul(_shares)).div(totalSupply());
+        _burn(msg.sender, _shares);
+
+        // Transfer the token
+        token.safeTransfer(msg.sender, r);
+    }
+
+    function getPricePerFullShare() public view returns (uint256) {
+        return balance().mul(1e18).div(totalSupply());
+    }
+   
 }
