58b307302bb90746ba71b7eb044cd9d576cac6d8
andersonlee0725 <cetbdb23@gmail.com>
2021-08-13 01:17:17 -0700

    fix: get reward

--- contracts/ProfitablePool.sol
+++ contracts/ProfitablePool.sol
@@ -1,5 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0
-
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 interface IConvex {
@@ -12,26 +11,19 @@ interface ICurve {
 }
 
 contract ProfitablePool {
-  function getProfitablePool(address[] calldata convex, address curve) public view returns (address) {
-    uint maxValue = 0;
-    uint maxIndex = 0;
+  function getProfitablePool(address[] calldata convex, address curve) public view returns(uint[] memory rewards) {
+    rewards = new uint[](convex.length);
     uint rewardRate;
     uint totalSupply;
     uint virtualPrice;
-    uint rewards;
+    uint reward;
 
     for (uint i = 0; i < convex.length; i++) {
       rewardRate = IConvex(convex[i]).rewardRate();
       totalSupply = IConvex(convex[i]).totalSupply();
       virtualPrice = ICurve(curve).get_virtual_price();
-      rewards = totalSupply * virtualPrice / rewardRate;
-      rewards = type(uint).max / rewards;
-      if (rewards > maxValue) {
-        maxValue = rewards;
-        maxIndex = i;
-      }
+      reward = rewardRate * (10 ** 40) / totalSupply / virtualPrice;
+      rewards[i] = reward;
     }
-
-    return convex[maxIndex];
   }
 }
