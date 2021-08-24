cfcd3d2eb05466318fc3bfa0e79187afb9bbcba2
Alexey Senichev <senichevalexey@gmail.com>
2021-08-24 13:10:34 +0300

    Added deposit function
    The deposit function has been implemented, the lp token calculation functionality has also been added

--- contracts/ZunamiVault.sol
+++ contracts/ZunamiVault.sol
@@ -1,13 +1,70 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.0;
 
-import "hardhat/console.sol";
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
-contract ZunamiVault {
-    function deposit(address _depositer, uint _amount, bytes32 _ticker) external {}
+import 'hardhat/console.sol';
 
-    function withdrawAll(address _depositer, int128 _coin, uint _minAmount,
-        bytes32 _ticker) external {}
+import { AddressAndTickers as Constants } from './helpers/AddressAndTickers.sol';
 
-    function withdraw(address _depositer, uint _amount, bytes32 _ticker) external {}
+contract ZunamiVault is ERC20 {
+    IERC20 immutable dai;
+    IERC20 immutable usdc;
+    IERC20 immutable usdt;
+
+    constructor() ERC20('zunamiLP', 'zlpt') {
+        dai = IERC20(Constants.DAI_ADDRESS);
+        usdc = IERC20(Constants.USDC_ADDRESS);
+        usdt = IERC20(Constants.USDT_ADDRESS);
+    }
+
+    function deposit(
+        uint256 amountOfDAI,
+        uint256 amountOfUSDC,
+        uint256 amountOfUSDT
+    ) external {
+        require(dai.balanceOf(msg.sender) >= amountOfDAI, 'Insufficient funds');
+        require(usdc.balanceOf(msg.sender) >= amountOfUSDC, 'Insufficient funds');
+        require(usdt.balanceOf(msg.sender) >= amountOfUSDT, 'Insufficient funds');
+
+        uint256 totalStablecoinsAmount = amountOfDAI + amountOfUSDC + amountOfUSDT;
+
+        dai.transferFrom(msg.sender, address(this), amountOfDAI);
+        usdc.transferFrom(msg.sender, address(this), amountOfUSDC);
+        usdt.transferFrom(msg.sender, address(this), amountOfUSDT);
+
+        uint256 shares = 0;
+        uint256 totalSupply = totalSupply();
+        uint256 totalStablecoinsInPool = _totalDepositedStablecoins();
+
+        if (totalSupply == 0) {
+            shares = totalStablecoinsAmount;
+        } else {
+            shares = (totalStablecoinsAmount * totalSupply) / totalStablecoinsInPool;
+        }
+
+        _mint(msg.sender, shares);
+    }
+
+    function withdrawAll(
+        address _depositer,
+        int128 _coin,
+        uint256 _minAmount,
+        bytes32 _ticker
+    ) external {}
+
+    function withdraw(
+        address _depositer,
+        uint256 _amount,
+        bytes32 _ticker
+    ) external {}
+
+    function _totalDepositedStablecoins() private view returns (uint256 balanceOfStablecoins) {
+        uint256 daiBalance = dai.balanceOf(address(this));
+        uint256 usdcBalance = usdc.balanceOf(address(this));
+        uint256 usdtBalance = usdt.balanceOf(address(this));
+
+        return daiBalance + usdcBalance + usdtBalance;
+    }
 }
