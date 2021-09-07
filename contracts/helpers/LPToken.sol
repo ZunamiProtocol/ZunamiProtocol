e3872c7f1f98b0cce9464f96f8f698c2c88c6cce
Alexey Senichev <senichevalexey@gmail.com>
2021-09-07 15:53:52 +0300

    Prepare test environment

--- contracts/helpers/LPToken.sol
+++ contracts/helpers/LPToken.sol
@@ -5,7 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol"
 
 contract LPToken is ERC20PresetMinterPauser {
     constructor(string memory name, string memory symbol)
-        public
         ERC20PresetMinterPauser(name, symbol)
     {}
 }
--- test/Zunami.test.ts
+++ test/Zunami.test.ts
@@ -1,42 +1,62 @@
-import { ethers } from "hardhat";
-import { expect } from "chai";
+import { ethers } from 'hardhat';
+import { waffle } from 'hardhat';
+import { expect } from 'chai';
 
-const SUPPLY = "100000000000000";
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { ContractFactory } from 'ethers';
+import { Contract } from '@ethersproject/contracts';
+
+const SUPPLY = '100000000000000';
 const ADMIN_ROLE =
-    "0x0000000000000000000000000000000000000000000000000000000000000000";
-const { waffle } = require("hardhat");
+    '0x0000000000000000000000000000000000000000000000000000000000000000';
 const provider = waffle.provider;
 
-describe("Zunami", function () {
-    before(async function () {
-        this.signers = await ethers.getSigners();
-        this.owner = this.signers[0];
-        this.alice = this.signers[1];
-        this.bob = this.signers[2];
-        this.carol = this.signers[3];
+describe('Zunami', function () {
+    let owner: SignerWithAddress;
+    let alice: SignerWithAddress;
+    let bob: SignerWithAddress;
+    let carol: SignerWithAddress;
 
-        this.LPToken = await ethers.getContractFactory("LPToken");
-        this.Zunami = await ethers.getContractFactory("Zunami");
-        this.CurveAaveConvex = await ethers.getContractFactory(
-            "CurveAaveConvex"
-        );
+    let LPToken: ContractFactory;
+    let Zunami: ContractFactory;
+    let CurveAaveConvex: ContractFactory;
+    let lp: Contract;
+    let zunami: Contract;
+    let strategy: Contract;
+    let referenceBlock: number;
+
+    before(async function () {
+        [owner, alice, bob, carol] = await ethers.getSigners();
+
+        LPToken = await ethers.getContractFactory('LPToken');
+        Zunami = await ethers.getContractFactory('Zunami');
+        CurveAaveConvex = await ethers.getContractFactory('CurveAaveConvex');
     });
 
     beforeEach(async function () {
-        this.lp = await this.LPToken.deploy("LP", "LP");
-        await this.lp.deployed();
-        await this.lp.mint(this.owner.address, SUPPLY);
-        this.zunami = await this.Zunami.deploy(this.lp.address);
-        await this.zunami.deployed();
-        await this.lp.grantRole(ADMIN_ROLE, this.zunami.address);
-        this.strategy = await this.CurveAaveConvex.deploy();
-        await this.strategy.deployed();
-        this.referenceBlock = await provider.getBlockNumber();
+        lp = await LPToken.deploy('LP', 'LP');
+        await lp.deployed();
+        await lp.mint(owner.address, SUPPLY);
+
+        zunami = await Zunami.deploy(lp.address);
+        await zunami.deployed();
+
+        await lp.grantRole(ADMIN_ROLE, zunami.address);
+
+        strategy = await CurveAaveConvex.deploy();
+        await strategy.deployed();
+
+        referenceBlock = await provider.getBlockNumber();
     });
 
-    it("should correctly init contracts", async function () {
-        expect(await this.lp.balanceOf(this.owner.address)).to.be.equal(SUPPLY);
-        const token = await this.strategy.tokens(0);
-        expect(token).to.equal("0x6B175474E89094C44Da98b954EedeAC495271d0F");
+    it('should correctly init contracts', async () => {
+        expect(await lp.balanceOf(owner.address)).to.be.equal(SUPPLY);
+
+        const token: string = await strategy.tokens(0);
+        expect(token).to.equal('0x6B175474E89094C44Da98b954EedeAC495271d0F');
+    });
+
+    it('deposit assets and get lp', async () => {
+        zunami.deposit;
     });
 });
