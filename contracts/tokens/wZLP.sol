65e59e7815755d554fb8799a855bd49123865784
goblin007 <ltrend@yandex.ru>
2022-01-11 12:29:26 +0700

    wZLP

---
+++ contracts/tokens/wZLP.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+
+contract wZLPtoken is Context, Ownable, ERC20 {
+
+    constructor() ERC20('wZLP', 'wZLP') {}
+
+    using SafeMath for uint256;
+    using Address for address;
+
+    function mint(address _to, uint256 _amount) public onlyOwner {
+        _mint(_to, _amount);
+    }
+}
