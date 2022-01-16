8b506e2b10a3dca23b0070d6b53e8adb82e2e0ab
goblin007 <ltrend@yandex.ru>
2022-01-16 23:19:20 +0700

    change burn vezun token interface

--- contracts/interfaces/IVeZunToken.sol
+++ contracts/interfaces/IVeZunToken.sol
@@ -4,5 +4,5 @@ pragma solidity 0.8.4;
 interface IVeZunToken {
     function mint(address _to, uint256 amount) external;
 
-    function burn(address _from, uint256 amount) external;
+    function burn(uint256 amount) external;
 }
