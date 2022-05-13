9d07587acfbd37a87214080a4e96bdf02dcef920
MioGreen <mikhail.zelenin@gmail.com>
2022-05-13 12:00:06 +0300

    Improve rebalancing calculator

--- scripts/rebalancePoolsHoldings.ts
+++ scripts/rebalancePoolsHoldings.ts
@@ -1,11 +1,13 @@
 import { ethers } from 'hardhat';
 import { BigNumber } from 'bignumber.js';
 
-interface IPoolInfo {
+interface IPoolValueInfo {
+    address: string;
     holdings: string;
     tokens: string;
     price: string;
     additionalHoldings: string;
+    rebalancedPrice: string;
 }
 
 async function main() {
@@ -19,38 +21,51 @@ async function main() {
     const provider = new ethers.providers.JsonRpcProvider(process.env.NODE_API_KEY);
     const zunami = new ethers.Contract(zunamiAddr, zunamiAbi, provider);
 
-    const poolCount = await zunami.poolCount();
-    const zlpPrice: BigNumber = new BigNumber(+(await zunami.lpPrice()));
-    const pools: IPoolInfo[] = [];
+    const poolCount = +(await zunami.poolCount());
+    const poolIds = Array.from(Array(poolCount).keys());
+    const poolInfos = await Promise.all(poolIds.map((id) => zunami.poolInfo(id)));
+
+    const [zlpPriceInt, holdingsPoolsInt] = await Promise.all([
+      zunami.lpPrice(),
+      Promise.all(poolIds.map(async (id) => (new ethers.Contract(poolInfos[id][0], strategyAbi, provider)).totalHoldings()))
+    ]);
+
+    const zlpPrice: BigNumber = new BigNumber(+zlpPriceInt).dividedBy(1e18);
+    const pools: IPoolValueInfo[] = [];
 
     for (let i = 0; i < poolCount; i++) {
-        let [addr, , tokens] = await zunami.poolInfo(i);
-        let strategy = new ethers.Contract(addr, strategyAbi, provider);
-        let holdings = new BigNumber(+(await strategy.totalHoldings()));
+        let poolZlpAmount = new BigNumber(+poolInfos[i][2]).dividedBy(1e18);
+        let holdings = new BigNumber(+holdingsPoolsInt[i]).dividedBy(1e18);
+        let stratAddr = poolInfos[i][0];
+        stratAddr = stratAddr.substring(0,8) + '...' +  stratAddr.substring(stratAddr.length - 4)
 
         if (holdings.eq(0)) {
             pools.push({
+                address: stratAddr,
                 holdings: '0',
                 tokens: '0',
                 price: '0',
                 additionalHoldings: '0',
+                rebalancedPrice: '0',
             });
             continue;
         }
 
         let price = holdings
-            .dividedBy(+tokens)
-            .times(1e18)
-            .integerValue();
+            .dividedBy(+poolZlpAmount);
 
         let deltaPrice = zlpPrice.minus(price);
-        let additionalHoldings = holdings.dividedBy(price).times(deltaPrice).dividedBy(1e18);
+        let additionalHoldings = holdings.dividedBy(price).times(deltaPrice);
+
+        let rebalancedPrice = holdings.plus(additionalHoldings).dividedBy(+poolZlpAmount);
 
         pools.push({
+            address: stratAddr,
             holdings: holdings.toString(),
-            tokens: tokens.toString(),
+            tokens: poolZlpAmount.toString(),
             price: price.toString(),
             additionalHoldings: additionalHoldings.toString(),
+            rebalancedPrice: rebalancedPrice.toString(),
         });
     }
 
