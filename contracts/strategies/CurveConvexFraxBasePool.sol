482ebafb161e7274bbdda69e72f3d6ce937442e6
Alexey <senichevalexey@gmail.com>
2022-08-04 22:11:31 +0800

    Created a FraxBase strategy

--- contracts/strategies/CurveConvexFraxBasePool.sol
+++ contracts/strategies/CurveConvexFraxBasePool.sol
@@ -1,9 +1,23 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import '@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol';
+import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
+
 import './CurveConvexExtraStratBase.sol';
+import '../utils/Constants.sol';
+
+import '../interfaces/ICurvePool2.sol';
 
 abstract contract CurveConvexFraxBasePool is CurveConvexExtraStratBase {
+    using SafeERC20 for IERC20Metadata;
+
+    ICurvePool2 public pool;
+    IERC20Metadata public lpToken;
+
+    int128 CURVE_POOL_USDC_ID = 1;
+    uint256 USDC_ID = 1;
+
     constructor(
         Config memory config,
         address poolLPAddr,
@@ -12,14 +26,119 @@ abstract contract CurveConvexFraxBasePool is CurveConvexExtraStratBase {
         address tokenAddr,
         address extraRewardsAddr,
         address extraTokenAddr
-    ) CurveConvexExtraStratBase(
-        config,
-        poolLPAddr,
-        rewardsAddr,
-        poolPID,
-        tokenAddr,
-        extraRewardsAddr,
-        extraTokenAddr,
-        [Constants.WETH_ADDRESS, Constants.USDC_ADDRESS]
-    ) { }
+    )
+        CurveConvexExtraStratBase(
+            config,
+            poolLPAddr,
+            rewardsAddr,
+            poolPID,
+            tokenAddr,
+            extraRewardsAddr,
+            extraTokenAddr,
+            [Constants.WETH_ADDRESS, Constants.USDC_ADDRESS]
+        )
+    {}
+
+    function checkDepositSuccessful(uint256[3] memory tokenAmounts)
+        internal
+        view
+        override
+        returns (bool isValidDepositAmount)
+    {
+        uint256 amountsTotal = tokenAmounts[USDC_ID] * decimalsMultipliers[USDC_ID];
+
+        uint256 amountsMin = (amountsTotal * minDepositAmount) / DEPOSIT_DENOMINATOR;
+        uint256 lpPrice = pool.get_virtual_price();
+
+        uint256[2] memory amounts;
+        amounts[USDC_ID] = tokenAmounts[USDC_ID];
+        uint256 depositedLp = pool.calc_token_amount(amounts, true);
+
+        isValidDepositAmount = (depositedLp * lpPrice) / CURVE_PRICE_DENOMINATOR >= amountsMin;
+    }
+
+    function depositPool(uint256[3] memory tokenAmounts)
+        internal
+        override
+        returns (uint256 lpTokenAmount)
+    {
+        _config.tokens[USDC_ID].safeIncreaseAllowance(address(pool), tokenAmounts[USDC_ID]);
+
+        uint256[2] memory amounts;
+        amounts[USDC_ID] = tokenAmounts[USDC_ID];
+
+        lpTokenAmount = pool.add_liquidity(amounts, 0);
+        poolLP.safeApprove(address(_config.booster), lpTokenAmount);
+        _config.booster.depositAll(cvxPoolPID, true);
+    }
+
+    function getCurvePoolPrice() internal view override returns (uint256 curveVirtualPrice) {
+        curveVirtualPrice = pool.get_virtual_price();
+    }
+
+    function calcWithdrawOneCoin(uint256 userRatioOfCrvLps, uint128 tokenIndex)
+        external
+        view
+        override
+        returns (uint256 tokenAmount)
+    {
+        uint256 removingCrvLps = (cvxRewards.balanceOf(address(this)) * userRatioOfCrvLps) / 1e18;
+        tokenAmount = pool.calc_withdraw_one_coin(removingCrvLps, CURVE_POOL_USDC_ID);
+    }
+
+    function calcSharesAmount(uint256[3] memory tokenAmounts, bool isDeposit)
+        external
+        view
+        override
+        returns (uint256 sharesAmount)
+    {
+        uint256[2] memory amounts;
+        amounts[USDC_ID] = tokenAmounts[USDC_ID];
+        sharesAmount = pool.calc_token_amount(amounts, isDeposit);
+    }
+
+    function calcCrvLps(
+        WithdrawalType withdrawalType,
+        uint256 userRatioOfCrvLps, // multiplied by 1e18
+        uint256[3] memory tokenAmounts,
+        uint128 tokenIndex
+    )
+        internal
+        view
+        override
+        returns (
+            bool success,
+            uint256 removingCrvLps,
+            uint256[] memory tokenAmountsDynamic
+        )
+    {
+        removingCrvLps = (cvxRewards.balanceOf(address(this)) * userRatioOfCrvLps) / 1e18;
+
+        uint256[2] memory amount;
+        amount[USDC_ID] = tokenAmounts[USDC_ID];
+
+        success = removingCrvLps >= pool.calc_token_amount(amount, false);
+
+        if (success && withdrawalType == WithdrawalType.OneCoin) {
+            success =
+                tokenAmounts[USDC_ID] <=
+                pool.calc_withdraw_one_coin(removingCrvLps, CURVE_POOL_USDC_ID);
+        }
+
+        tokenAmountsDynamic = new uint256[](2);
+    }
+
+    function sellToken() public {
+        uint256 sellBal = token.balanceOf(address(this));
+        if (sellBal > 0) {
+            token.safeApprove(address(pool), sellBal);
+            pool.exchange_underlying(0, 3, sellBal, 0);
+        }
+    }
+
+    function withdrawAllSpecific() internal override {
+        uint256[2] memory minAmounts;
+        sellToken();
+        pool.remove_liquidity(lpToken.balanceOf(address(this)), minAmounts);
+    }
 }
