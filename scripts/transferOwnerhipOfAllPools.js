353e88031556b2fe0dcfeb67d0b56469887044cd
Mikhail Zelenin <mikhail.zelenin@gmail.com>
2022-10-08 13:44:16 +0300

    Transfer pools owner to multisig wallet

---
+++ scripts/transferOwnerhipOfAllPools.js
@@ -0,0 +1,33 @@
+
+
+async function main() {
+    const Zunami = await ethers.getContractFactory('Zunami');
+    const zunami = await Zunami.attach('0x2ffCC661011beC72e1A9524E12060983E74D14ce');
+
+    const Strat = await ethers.getContractFactory('RebalancingStrat');
+
+    await zunami.deployed();
+    console.log('Zunami deployed to:', zunami.address);
+
+    const newAdmin = '0xb056B9A45f09b006eC7a69770A65339586231a34';
+    const poolCount = await zunami.poolCount();
+    for (let i = 0; i <= poolCount; i++) {
+        const poolInfo = await zunami.poolInfo(i);
+        const stratAddr = poolInfo["strategy"];
+        const strat = await Strat.attach(stratAddr);
+        console.log("Strat id", i);
+        console.log("Strat addr", stratAddr);
+        console.log("Strat owner before", await strat.owner());
+        const tx = await strat.transferOwnership(newAdmin);
+        await tx.wait();
+        console.log("Strat owner after", await strat.owner());
+        console.log(" ");
+    }
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
