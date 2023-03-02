0e39f527bb1299acb6a5416b2324bc23c59d8bf8
Mikhail Zelenin <mikhail.zelenin@gmail.com>
2023-03-02 16:46:11 +0200

    Add Chainlink slippage verification for uniswap like reward manager

--- contracts/strategies/rewardManager/SellingUniswapRewardManager.sol
+++ contracts/strategies/rewardManager/SellingUniswapRewardManager.sol
@@ -7,18 +7,38 @@ import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 import '../../utils/Constants.sol';
 import '../../interfaces/IUniswapRouter.sol';
 import '../interfaces/IRewardManager.sol';
+import "../interfaces/AggregatorV2V3Interface.sol";
 
 contract SellingUniswapRewardManager is IRewardManager {
     using SafeERC20 for IERC20Metadata;
 
+    uint256 public constant SLIPPAGE_DENOMINATOR = 10_000;
+
+    uint256 public defaultSlippage = 300; // 3%
+
     IUniswapRouter public router;
     address public middleSwapToken;
+
+    mapping(address => address) public rewardUsdChainlinkOracles;
 
     constructor(address routerAddr, address middleSwapTokenAddr) {
         require(routerAddr != address(0), 'Zero router');
         require(middleSwapTokenAddr != address(0), 'Zero middle swap token');
         router = IUniswapRouter(routerAddr);
         middleSwapToken = middleSwapTokenAddr;
+
+        rewardUsdChainlinkOracles[
+            Constants.CVX_ADDRESS
+        ] = 0xd962fC30A72A84cE50161031391756Bf2876Af5D; // https://data.chain.link/ethereum/mainnet/crypto-usd/cvx-usd
+        rewardUsdChainlinkOracles[
+            Constants.CRV_ADDRESS
+        ] = 0xCd627aA160A6fA45Eb793D19Ef54f5062F20f33f; // https://data.chain.link/ethereum/mainnet/crypto-usd/crv-usd
+        rewardUsdChainlinkOracles[
+            Constants.FXS_ADDRESS
+        ] = 0x6Ebc52C8C1089be9eB3945C4350B68B8E4C2233f; // https://data.chain.link/ethereum/mainnet/crypto-usd/fxs-usd
+        rewardUsdChainlinkOracles[
+            Constants.SPELL_ADDRESS
+        ] = 0x8c110B94C5f1d347fAcF5E1E938AB2db60E3c9a8; // https://data.chain.link/ethereum/mainnet/crypto-usd/spell-usd
     }
 
     function handle(
@@ -29,13 +49,16 @@ contract SellingUniswapRewardManager is IRewardManager {
         if (amount == 0) return;
 
         IERC20Metadata(reward).safeApprove(address(router), amount);
-        router.swapExactTokensForTokens(
+        
+        uint256[] memory amounts = router.swapExactTokensForTokens(
             amount,
             0,
             fromAddressArr3([reward, middleSwapToken, feeToken]),
             msg.sender,
             block.timestamp + Constants.TRADE_DEADLINE
         );
+
+        checkSlippage(reward, amount, amounts[amounts.length - 1]);
     }
 
     function valuate(
@@ -56,6 +79,21 @@ contract SellingUniswapRewardManager is IRewardManager {
         if (amount == 0) return 0;
         uint256[] memory amounts = router.getAmountsOut(amount, exchangePath);
         return amounts[amounts.length - 1];
+    }
+
+    function checkSlippage(
+        address reward,
+        uint256 amount,
+        uint256 feeTokenAmount
+    ) internal view {
+        AggregatorV2V3Interface oracle = AggregatorV2V3Interface(rewardUsdChainlinkOracles[reward]);
+        (, int256 answer, , , ) = oracle.latestRoundData();
+
+        uint256 feeTokenAmountByOracle = (uint256(answer) * amount) / 1e20; // reward decimals 18 + oracle decimals 2 (8 - 6)
+        uint256 feeTokenAmountByOracleWithSlippage = (feeTokenAmountByOracle *
+        (SLIPPAGE_DENOMINATOR - defaultSlippage)) / SLIPPAGE_DENOMINATOR;
+
+        require(feeTokenAmount >= feeTokenAmountByOracleWithSlippage, 'Wrong slippage');
     }
 
     function fromAddressArr3(address[3] memory arr)
