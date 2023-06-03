eaf0dbb693ed3a92338c6b3c1a20d78bb9b91790
Mikhail Zelenin <mikhail.zelenin@gmail.com>
2023-06-03 08:46:50 +0300

    Deploy eUSD Convex staking strategy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -81,7 +81,7 @@ const config: HardhatUserConfig = {
             chainId: 1,
             gas: 'auto',
             gasMultiplier: 1.2,
-            gasPrice: 80000000000,
+            gasPrice: 21000000000,
             accounts: [`${process.env.PRIVATE_KEY}`],
             loggingEnabled: true,
         },
